584519c09e9eab2666269b1353894cb7e49eb4dd
rimonc7 <rimonc7@gmail.com>
2025-01-20 09:31:02 +0600

    nav design done

--- src/Pages/Shared/Nav.jsx
+++ src/Pages/Shared/Nav.jsx
@@ -1,73 +1,100 @@
 import { FaUserCircle } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
-    const user = true;
+    const user = false;
 
     const links = (
-        <div className="space-y-2 lg:space-x-3 lg:space-y-0 uppercase">
-            <NavLink to="/" className="block lg:inline">HOME</NavLink>
-            <NavLink to="/contact" className="block lg:inline">CONTACT US</NavLink>
-            <NavLink to="/menu" className="block lg:inline">OUR MENU</NavLink>
-            <NavLink to="/shop/salad" className="block lg:inline">OUR SHOP</NavLink>
+        <div className="space-y-2 lg:space-x-4 lg:space-y-0 uppercase">
+            <NavLink to="/" className="block lg:inline text-white hover:text-orange-300">
+                HOME
+            </NavLink>
+            <NavLink to="/contact" className="block lg:inline text-white hover:text-orange-300">
+                CONTACT US
+            </NavLink>
+            <NavLink to="/menu" className="block lg:inline text-white hover:text-orange-300">
+                OUR MENU
+            </NavLink>
+            <NavLink to="/shop/salad" className="block lg:inline text-white hover:text-orange-300">
+                OUR SHOP
+            </NavLink>
         </div>
     );
 
     const authLinks = (
-        <div className="space-y-2 lg:space-x-3 lg:space-y-0 uppercase">
-            <>
-                <button className="block lg:inline">Logout</button>
-                <NavLink to="/login" className="block lg:inline">Login</NavLink>
-                <NavLink to="/signUp" className="block lg:inline">Sign Up</NavLink>
-            </>
+        <div className="space-y-2 lg:space-x-4 lg:space-y-0 uppercase">
+            {user ? (
+                <button className="block lg:inline btn btn-sm btn-outline border-white text-white hover:bg-orange-300">
+                    Logout
+                </button>
+            ) : (
+                <>
+                    <NavLink
+                        to="/login"
+                        className="block lg:inline btn btn-sm btn-outline border-white text-white hover:bg-orange-300"
+                    >
+                        Login
+                    </NavLink>
+                    <NavLink
+                        to="/signUp"
+                        className="block lg:inline btn btn-sm btn-outline border-white text-white hover:bg-orange-300"
+                    >
+                        Sign Up
+                    </NavLink>
+                </>
+            )}
         </div>
-    )
+    );
 
     return (
-        <div className="navbar fixed z-10 bg-black text-white bg-opacity-30 max-w-screen-xl">
+        <div className="navbar fixed z-10 bg-gradient-to-r from-blue-500 to-orange-400 text-white shadow-lg">
             <div className="navbar-start">
+                {/* Dropdown for small screens */}
                 <div className="dropdown lg:hidden">
-                    <div tabIndex={0} role="button" className="btn btn-ghost">
+                    <button tabIndex={0} className="btn btn-ghost">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
                             fill="none"
                             viewBox="0 0 24 24"
-                            stroke="currentColor">
+                            stroke="currentColor"
+                        >
                             <path
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 strokeWidth="2"
-                                d="M4 6h16M4 12h8m-8 6h16" />
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
                         </svg>
-                    </div>
+                    </button>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow text-black">
+                        className="menu menu-sm dropdown-content mt-3 w-52 bg-white text-black rounded-box shadow-lg"
+                    >
                         {links}
                         {authLinks}
                     </ul>
                 </div>
-                <a href="/" className="btn btn-ghost text-xl">DormDish</a>
+                <a href="/" className="btn btn-ghost text-xl font-bold uppercase">
+                    Dorm Dish
+                </a>
             </div>
+
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 space-x-3">
-                    {links}
-                </ul>
+                <ul className="menu menu-horizontal px-1 space-x-4">{links}</ul>
             </div>
+
             <div className="navbar-end">
-                <div className="text-sm hidden lg:flex">
-                    {authLinks}
-                </div>
+                <div className="hidden lg:flex">{authLinks}</div>
                 {user ? (
                     <img
-                        className="w-12 h-12 mx-6 rounded-full"
-                        src={user.photoURL}
-                        alt=""
-                        title={user.displayName || "User"}
+                        className="w-10 h-10 mx-4 rounded-full"
+                        src="/path-to-default-avatar.jpg"
+                        alt="User Avatar"
+                        title="Logged in User"
                     />
                 ) : (
-                    <FaUserCircle className="text-4xl mx-6" title="Guest User" />
+                    <FaUserCircle className="text-3xl mx-4" title="Guest User" />
                 )}
             </div>
         </div>
